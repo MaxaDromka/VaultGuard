e45085211e59bb6a18c91d3358d5ee6b369a5315
MaxaDromka <noskow.maximaxim@yandex.ru>
2025-04-06 11:57:18 +0300

    фикс запуска jar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.crypt {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.desktop;
     requires java.prefs;
     requires org.jnrproject.ffi;
